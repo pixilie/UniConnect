3815e7e203a2f896cc7313729d4b2017ffaa4902
Kristen Couty <kristen.couty@gmail.com>
2026-02-17 11:19:56 +0200

    feat: .env support & config

---
+++ server/app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings() # type: ignore
