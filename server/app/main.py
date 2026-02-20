13dd7936090a3515a6efe8b0abf462dfb6278cc8
Kristen Couty <kristen.couty@gmail.com>
2026-02-20 02:34:55 +0200

    feat: new routes

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.db.database import Base, engine
-from app.routers import assignment, users
+from app.routers import admin, assignment, group, schedule, user
 
 # Database init
 Base.metadata.create_all(bind=engine)
@@ -9,8 +9,11 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Registering routes
-app.include_router(users.user_router, prefix="/api", tags=["Users"])
+app.include_router(user.user_router, prefix="/api", tags=["Users"])
 app.include_router(assignment.assignment_router, prefix="/api", tags=["Assignments"])
+app.include_router(schedule.schedule_router, prefix="/api", tags=["Schedules"])
+app.include_router(admin.admin_router, prefix="/api", tags=["Administrator"])
+app.include_router(group.group_router, prefix="/api", tags=["Groups"])
 
 @app.get("/")
 def read_root():
