ff09ef66c1293630dab529d7daee7aaf49a5c6ba
Kristen Couty <kristen.couty@gmail.com>
2026-03-03 10:55:11 +0200

    feat: postgresql compatibility

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -3,13 +3,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.core.config import settings
 
-# connect_args={"check_same_thread": False} specific to SQLite
+is_sqlite = settings.DATABASE_URL.startswith("sqlite")
+connect_args = {"check_same_thread": False} if is_sqlite else {}
+
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL,
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
