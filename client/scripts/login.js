1e9082c7aff0aa91b5747843e7b7300e32bd941d
Fourmisssss <spahn.laurick@gmail.com>
2026-03-07 16:57:04 +0100

    feat(client): login page client-server logic
    Co-authored-by: Kristen Couty <kristen.couty@gmail.com>
    Co-authored-by: Fourmisssss <>

---
+++ client/scripts/login.js
@@ -0,0 +1,45 @@
+async function login(username, password) {
+  const params = new URLSearchParams();
+  params.append("grant_type", "password");
+  params.append("username", username);
+  params.append("password", password);
+
+  try {
+    const res = await fetch("https://uniconnect.pixilie.net/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params
+    });
+
+    if (!res.ok) return false;
+
+    const data = await res.json();
+    localStorage.setItem("token", data.access_token);
+    return true;
+
+  } catch (err) {
+    return false;
+  }
+}
+
+document.getElementById("loginForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  console.log("script running ");
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  let message = document.getElementById("message");
+  let success = await login(email, password);
+
+  if (success) {
+    message.style.color = "green";
+    message.innerText = "Login Successful!";
+    window.location.href = "../pages/chat.html";
+  }
+  else {
+    message.style.color = "red";
+    message.innerText = "Invalid email or password";
+  }
+
+});
