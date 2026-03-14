55c3e708d31febab16b1b07111071d3f0beec443
Kristen Couty <kristen.couty@gmail.com>
2026-03-14 17:29:03 +0200

    fix: waiting a bit before trying to connect to db

--- app/db/database.py
+++ app/db/database.py
@@ -1,22 +1,33 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from psycopg2 import OperationalError as Psycopg2OpError
+from sqlalchemy.exc import OperationalError
 
 from app.core.config import settings
 
 is_sqlite = settings.DATABASE_URL.startswith("sqlite")
 connect_args = {"check_same_thread": False} if is_sqlite else {}
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args=connect_args
-)
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
+MAX_RETRIES = 5
+RETRY_DELAY = 2
+
+for i in range(MAX_RETRIES):
     try:
-        yield db
-    finally:
-        db.close()
+        time.sleep(RETRY_DELAY)
+        with engine.connect() as connection:
+            print("🟢 Connection to DB established")
+    except (OperationalError, Psycopg2OpError):
+        print(
+            f"🟡 DB not ready... Retry in {RETRY_DELAY} seconds ({i + 1}/{MAX_RETRIES})"
+        )
+        time.sleep(RETRY_DELAY)
+else:
+    print(
+        "🔴 Impossible to connect to DB"
+    )
