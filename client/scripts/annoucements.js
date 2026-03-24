a9f09491e3e2f7d4da71bbc319332c66f996cd6a
Kristen Couty <kristen.couty@gmail.com>
2026-03-24 16:33:20 +0200

    fix(client): announcement page

--- client/scripts/annoucements.js
+++ client/scripts/annoucements.js
@@ -1,15 +1,14 @@
 requireAuth();
 const annoucementTemplate = document.getElementById("announcementTemplate");
-const displayBox=document.getElementById("displayBox");
-const createBtn=document.getElementById("sendBtn");
-const createForm=document.getElementById("form")
-const cancelBtn =document.getElementById("cancelBtn");
-const formTitle=document.getElementById("title-input");
-const formContent=document.getElementById("message-input");
-const formUrgent=document.getElementById("urgency-input");
-const sendBtn=document.getElementById("postBtn");
+const displayBox = document.getElementById("displayBox");
+const createBtn = document.getElementById("sendBtn");
+const createForm = document.getElementById("form")
+const cancelBtn = document.getElementById("cancelBtn");
+const formTitle = document.getElementById("title-input");
+const formContent = document.getElementById("message-input");
+const formUrgent = document.getElementById("urgency-input");
+const sendBtn = document.getElementById("postBtn");
 
-console.log(createBtn==null)
 async function LoadAnnoucements() {
     const currentGroupId = AppState.currentGroupId;
 
@@ -30,57 +29,61 @@ async function LoadAnnoucements() {
         let data = await res.json();
 
         data.forEach(element => {
-            let first_name = element.creator.first_name;
-            let last_name = element.creator.last_name;
-            //let role = element.creator.role;
-            let title=element.title;
-            let content=element.content;
-            let urgent=element.urgent;
-            addAnnoucement(first_name,last_name,role,title,content,data,urgent);
+            let first_name = element.author.first_name;
+            let last_name = element.author.last_name;
+            let role = element.author.role;
+            let title = element.title;
+            let content = element.content;
+            let urgent = element.urgent;
+            let date = element.sent_at
+            addAnnoucement(first_name, last_name, role, title, content, date, urgent);
         });
     }
 }
 
-function addAnnoucement(first_name,last_name,role,title,content,date,urgent){
-    const annoucementsNode = annoucementTemplate.content.cloneNode(true);
+function addAnnoucement(first_name, last_name, role, title, content, date, urgent) {
+    const annoucementsNode = annoucementTemplate.content.cloneNode(true).firstElementChild;
 
     let f = "";
     if (first_name.length > 0) f = first_name[0].toUpperCase();
     let l = "";
     if (last_name.length > 0) l = last_name[0].toUpperCase();
 
-    const tempdate = new Date(date);
+    const tempDate = new Date(date);
 
     const options = {
         month: 'short',
-        day: 'numeric',  
-        year: 'numeric', 
-        hour: 'numeric',  
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
         minute: 'numeric',
-        hour12: true      
+        hour12: true
     };
 
-    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(tempdate);
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(tempDate);
 
     annoucementsNode.querySelector(".announcement-avatar").textContent = `${f}${l}`;
     annoucementsNode.querySelector(".announcement-user").textContent = `${first_name} ${last_name}`;
-    annoucementsNode.querySelector(".announcement-date").textContent=formattedDate;
+    annoucementsNode.querySelector(".announcement-date").textContent = formattedDate;
     annoucementsNode.querySelector(".annoucement-title").textContent = title;
     annoucementsNode.querySelector(".announcement-content").textContent = content;
-    //annoucementsNode.querySelector(".announcement-role").textContent = role.toUpperCase();
-    if(urgent)annoucementsNode.classList.add("urgent");
+    annoucementsNode.querySelector(".announcement-role").textContent = role.toUpperCase();
+
+    if (urgent) annoucementsNode.classList.add("urgent");
 
     displayBox.appendChild(annoucementsNode);
-    displayBox.scrollTop = chat.scrollHeight;
+    displayBox.scrollTop = displayBox.scrollHeight;
 }
 
 async function createAnnoucement() {
-    let title=formTitle.value;
-    formTitle.value="";
-    let content=formContent.value;
-    formContent.value="";
-    let urgency=formUrgent.checked;
-    formUrgent.checked=false;
+    const currentGroupId = AppState.currentGroupId;
+
+    let title = formTitle.value;
+    formTitle.value = "";
+    let content = formContent.value;
+    formContent.value = "";
+    let urgency = formUrgent.checked;
+    formUrgent.checked = false;
 
     await fetch(`${API_BASE_URL}/groups/${currentGroupId}/announcement`, {
         method: "POST",
@@ -104,15 +107,15 @@ cancelBtn.addEventListener("click", () => {
     createForm.classList.remove("form-active");
 });
 
-sendBtn.addEventListener("click", async ()=>{
-    //await createAnnoucement();
+sendBtn.addEventListener("click", async () => {
+    await createAnnoucement();
     createForm.classList.remove("form-active");
-    displayBox.innerHTML="";
-    //LoadAnnoucements();
+    displayBox.innerHTML = "";
+    LoadAnnoucements();
 });
 
 if (AppState.currentGroupId) {
     setTimeout(() => {
-        //LoadAnnoucements();
+        LoadAnnoucements();
     }, 100);
 }
